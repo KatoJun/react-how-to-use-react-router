7a5040b6c89dcc07314cc9061b74649a428fc1bd
KatoJun <jun.kato0208@gmail.com>
2021-10-25 16:41:13 +0900

    クエリパラメーターまで

--- src/Page2.jsx
+++ src/Page2.jsx
@@ -4,7 +4,9 @@ export const Page2 = () => {
   return (
     <div>
       <h1>Page2のページです</h1>
-      <Link to="/page2/999">URL component</Link>
+      <Link to="/page2/999">URL parameter</Link>
+      <br />
+      <Link to="/page2/999?name=hogehoge">Query parameter</Link>
     </div>
   );
 };
